614d43cfb54ab5d41899dfc9c6e690e8cc0bc52b
ritheasen <srenghanritheasen21@kit.edu.kh>
2022-07-01 11:31:09 +0700

    try

--- week01/ex/12_stringslice.py
+++ week01/ex/12_stringslice.py
@@ -1,6 +1,6 @@
 inputString = input("Enter a string: ")
-String1st = inputString[:len(inputString)//2]
-String2nd = inputString[len(inputString)//2:]
+String1st = inputString[0:len(inputString)//2]
+String2nd = inputString[len(inputString)//2:len(inputString)]
 
 if inputString == '':
     print('The string is empty.')
