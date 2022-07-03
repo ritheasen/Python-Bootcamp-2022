6c37ec1db7065f9881daa8cf6a76b94a3873f6fe
ritheasen <srenghanritheasen21@kit.edu.kh>
2022-07-03 10:10:55 +0700

    try

--- week01/ex/20_encode_decode.py
+++ week01/ex/20_encode_decode.py
@@ -1,19 +1,39 @@
 
 userEnter = int(input("Press 1 to encode \nPress 2 to decode"))
+#userYN = str(input("Do you want to continue?[Y/N]"))
 newString = ""
-if userEnter == 1:
-    inputString = str(input("Enter the string to encode"))
-    for i in inputString:
-        print(f"{i} : {ord(i)}")
-        if ord(i) > 77:
-            ord(i)-13
-            newString = ""
-        else:
-            ord(i)+13
-            newString = ""
 
-        #print(f"{chr(ord(i)+13)}")
-#print(f"{chr(ord(i)+13)}",end="")
-print(newString)
-#elif userEnter == 2:
+
+if userEnter == 1:
+    while True:
+        inputString = str(input("Enter the string to encode"))
+        for i in inputString:
+            B = ord(i) - 13
+            S = ord(i) + 13
+            if ord(i) > 77:
+                newString += chr(B)
+            else:
+                newString += chr(S)
+        print(f"The decode text is :{newString}\nDo you want to continue?[Y/N]")
+
+#print(userYN)
+
+
+#     if userEnter == 'Y':
+#         print("")
+#
+# if userEnter == 2:
+#     inputString = str(input("Enter the string to decode"))
+#     for i in inputString:
+#         B = ord(i) - 13
+#         S = ord(i) + 13
+#         if ord(i) > 77:
+#             newString += chr(B)
+#         else:
+#             newString += chr(S)
+#     print(newString)
+#         #print(f"{chr(ord(i)+13)}")
+# #print(f"{chr(ord(i)+13)}",end="")
+# print(newString)
+# #elif userEnter == 2:
 
--- week01/ex/test.py
+++ week01/ex/test.py
@@ -1,14 +1,31 @@
 
 
-sum = 0
 
-while True:
-    enterNumber = input("Input a number ")
-    if enterNumber == 'stop' :
-        break
-    elif enterNumber.isnumeric():
-        sum += int(enterNumber)
+
+inputString = str(input("Enter the string to encode"))
+newString = ""
+
+for i in inputString:
+    B = ord(i) - 13
+    S = ord(i) + 13
+    #print(f"{i} : {ord(i)}",end=",")
+    if ord(i) > 77:
+        #print(f"{ord(i)-13}")
+        #print(chr(B),end=" ")
+        newString += chr(B)
     else:
-        print("The input must be a valid number")
+        #print(f"{ord(i)+13}")
+        #print(chr(S))
+        newString += chr(S)
 
-print(f"Sum is : {sum}")
+
+print(newString)
+    #print(f"{chr(B)}+{chr(S)}")
+    # print(ord(i),end=",")
+    # print(i,end="")
+    # print(f"{chr(ord(i) + 13)}")
+    # print(f"{chr(ord(i) - 13)}")
+
+
+
+
