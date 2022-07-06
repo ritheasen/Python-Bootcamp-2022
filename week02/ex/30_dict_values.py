7f1917da3dac1faf6af0e57a18aaccec99ea76fc
ritheasen <srenghanritheasen21@kit.edu.kh>
2022-07-06 10:54:27 +0700

    commit

--- week02/ex/30_dict_values.py
+++ week02/ex/30_dict_values.py
@@ -1,19 +1,10 @@
 
 
 def dict_values(dictionary):
-    dictValue = dictionary.values()
-    dictKey = dictionary.keys()
     dictItem = dictionary.items()
-
     for dictKey, dictValue in dictItem:
-        print(f'{dictKey}:{dictValue}')
-    #print(*[str(dictValue) + ':' + str(dictKey) for dictValue, dictKey in dictionary.items()])
+        print(dictKey, ":",*dictValue)
         print(f"*****")
-
-    # print(dictValue)
-    # print(dictKey)
-    # print(newDict)
-    # print(dictItem)
 
 dict_values(({120: ("Visal", "Borey", "Sovann"),
               130: ("Hout","Mouyleng","Pidor"),
--- week02/ex/test.py
+++ week02/ex/test.py
@@ -1,21 +1,10 @@
 
 
 def dict_values(dictionary):
-    #dictValue = dictionary.values()
-    #dictKey = dictionary.keys()
-    # newDict = dict(zip(dictKey,dictValue))
     dictItem = dictionary.items()
-
     for dictKey, dictValue in dictItem:
-        print(f'{dictKey}:{dictValue}')
-        #print(dictKey, ":", dictValue)
-    #print(*[str(dictValue) + ':' + str(dictKey) for dictValue, dictKey in dictionary.items()])
+        print(dictKey, ":",*dictValue)
         print(f"*****")
-
-    # print(dictValue)
-    # print(dictKey)
-    # print(newDict)
-    # print(dictItem)
 
 dict_values(({120: ("Visal", "Borey", "Sovann"),
               130: ("Hout","Mouyleng","Pidor"),
